0dc0c8188885e998d104f70a4bd70b5e45973992
Tolga Duman <tolga387@hotmail.com>
2022-08-22 01:11:14 +0300

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName="Tolga"
+                },
+                new Student
+                {
+                    FirstName="Engin"
+                },
+                new Person
+                {
+                    FirstName="Ayşe"
+                }
+            };
+            foreach (var p in person)
+            {
+                Console.WriteLine(p.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
